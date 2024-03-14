1b27394444f47dc6fd27b80f2f16288d08b0c938
mshokarimo7 <mshokarim@gmail.com>
2024-03-14 22:51:46 +0500

    diff draw method

--- car.js
+++ car.js
@@ -61,25 +61,45 @@ class Car{
     
     update(roadBorders){
        this.#move();
+       this.polygon = this.#createPolygon();
        this.sensor.update(roadBorders);
+    }
+
+    #createPolygon(){
+        const points = [];
+        const rad = Math.hypot(this.width, this.height)/2;
+        const alpha = Math.atan2(this.width, this.height);
+        // top right point
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        });
+        // top left point
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        });
+        // bottom right point
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+        // bottom left point
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+
+        return points;
     }
     
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-        
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-            );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);7
+        for(let i = 1; i < this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-        ctx.restore();
-
         this.sensor.draw(ctx);
-
     }
 }
