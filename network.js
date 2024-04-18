5a4eafcefd5b80e0527f8c1fb50830ac7634d7ac
mshokarimo7 <mshokarim@gmail.com>
2024-04-18 14:24:46 +0500

    ml part

---
+++ network.js
@@ -0,0 +1,7 @@
+class Level{
+    constructor(inputCount, outputCount){
+        this.inputs = new Array(inputCount);
+        this.outputs = new Array(outputCount);
+        this.biases = new Array(outputCount);
+    }
+}
